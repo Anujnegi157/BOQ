1fe4dce65c2788c26bec6c0aef29d402ddfc9459
Anuj Singh Negi <anujnegi157@gmail.com>
2025-03-16 22:27:09 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import InputForm from "./Input";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <main className="flex flex-col items-center gap-8 py-16 max-w-[1280px] mx-auto">
